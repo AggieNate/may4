8c987d0cabc3677a9227691cf8781c7c83af7d02
Nate Young <nateyoung82@gmail.com>
2018-05-04 21:12:40 -0500

    Completed Pyramid Assignment

--- pyramid.py
+++ pyramid.py
@@ -1,42 +1,7 @@
 
 
-n = 17
+j = 17
 
-# function to create pyramid
-def triangle(n):
-	# This sets the number of spaces
-	spaces = 2 * n - 2
-
-	for i in range(0,n):
-		for j in range(0,spaces):
-			print(end=" ")
-		spaces = spaces - 1
-
-		for j in range(0, i+1):
-			print("* ", end="")
-
-		print("\r")
-
-triangle(n)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Assignment - Write a program to display a pyramid as shown below:
+for i in range(1,17,2):
+	print((" " * j) + (i * "*"))
+	j = j - 1
